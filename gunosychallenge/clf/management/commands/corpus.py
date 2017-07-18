fe014a71006b3e173f1d0cec46453e0c837fc08b
神宮亜良太 <arata@jinguuaryoutanoMacBook-Pro.local>
2017-07-18 19:59:01 +0900

    Add "-y" to "./bin/install-mecab-ipadic-neologd -n"

--- gunosychallenge/clf/management/commands/corpus.py
+++ gunosychallenge/clf/management/commands/corpus.py
@@ -7,21 +7,20 @@ from enum import Enum, auto
 class MethodEnum(Enum):
     NaiveBayes = auto()
     FastText = auto()
-        
+
 
 class Corpus:
     def __init__(self):
         self.pos = {"名詞", "形容詞"}
-        process = subprocess.run(["mecab-config --dicdir","'/mecab-ipadic-neologd'"],
-                                stdout=subprocess.PIPE,
-                                shell=True)
-        
+        process = subprocess.run(["mecab-config --dicdir", "'/mecab-ipadic-neologd'"],
+                                 stdout=subprocess.PIPE,
+                                 shell=True)
+
         dic_path = process.stdout.decode("utf-8").strip() + "/mecab-ipadic-neologd"
-        
+
         self.tagger_path = "-d " + dic_path
         self.wakati_tagger_path = "-Owakati -d " + dic_path
 
-    
     def corpus(self, records, mode):
         corpus = []
         if mode == MethodEnum.NaiveBayes:
@@ -30,33 +29,33 @@ class Corpus:
                 data["category"] = record["category"]
                 data["vocab"] = self.get_main_words(record["text"])
                 corpus.append(data)
-                
+
         elif mode == MethodEnum.FastText:
             for record in records:
                 data = {}
                 data["category"] = record["category"]
                 data["text"] = self.split_words(record["text"])
                 corpus.append(data)
-        
+
         else:
             return "Error: You can select 'NaiveBayes' or 'fasttext'"
-        
+
         return corpus
-        
+
     def get_main_words(self, text):
         out_words = []
         tagger = MeCab.Tagger(self.tagger_path)
         tagger.parse("")
         node = tagger.parseToNode(text)
-    
+
         while node:
             word_type = node.feature.split(",")[0]
             if word_type in self.pos:
                 out_words.append(node.surface)
-            node = node.next    
-        
+            node = node.next
+
         return out_words
-    
+
     def split_words(self, text):
         tagger = MeCab.Tagger(self.wakati_tagger_path)
         splitted_sent = tagger.parse(text)
--- gunosychallenge/clf/management/commands/make_clf.py
+++ gunosychallenge/clf/management/commands/make_clf.py
@@ -7,27 +7,25 @@ from clf.models import Article
 
 class Command(BaseCommand):
     help = "make classifier"
-    
+
     def add_arguments(self, parser):
         parser.add_argument("method", nargs="+")
-        
-    
+
     def handle(self, *args, **options):
-        
+
         if options["method"] == ["naivebayes"]:
             self.stdout.write(self.style.SUCCESS("naivebayes"))
-            
+
             data = Article.objects.values()
             corpus = Corpus().corpus(data, "NaiveBayes")
-            data_train, data_test = train_test_split(corpus, test_size=0.2 , random_state=42) 
+            data_train, data_test = train_test_split(corpus, test_size=0.2, random_state=42)
             nb = NaiveBayesClassifier()
             nb.train(data_train)
             nb.save()
             print("accuracy:", nb.score(data_test))
-            
+
             self.stdout.write(self.style.SUCCESS("Succesfully made classfier"))
-            
-            
+
         if options["method"] == ["doc2vec"]:
             self.stdout.write(self.style.SUCCESS("doc2vec"))
             self.stdout.write(self.style.SUCCESS("Succesfully made classfier"))
