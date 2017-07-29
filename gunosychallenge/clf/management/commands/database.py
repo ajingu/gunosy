84f672c8bf24db216bc257a05886eec72162854f
神宮亜良太 <arata@jinguuaryoutanoMacBook-Pro.local>
2017-07-29 11:05:28 +0900

    Add cross-validation function to NaiveBayesClassifier.

--- gunosychallenge/clf/management/commands/database.py
+++ gunosychallenge/clf/management/commands/database.py
@@ -5,9 +5,10 @@ import pymysql
 
 class Database:
     """Class that manages the interaction with the database."""
+
     def __init__(self):
         """Set cursor and connection."""
-        self.conn = pymysql.connect(os.environ["GUNOSY_DATABASE_NAME"],
+        self.conn = pymysql.connect(os.environ["GUNOSY__HOST"],
                                     os.environ["GUNOSY_USERNAME"],
                                     os.environ["GUNOSY_PASSWORD"],
                                     os.environ["GUNOSY_DATABASE_NAME"],
@@ -15,10 +16,13 @@ class Database:
 
         self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
 
+        self.table_name = os.environ["GUNOSY_TABLE_NAME"]
+
     def delete_data(self):
         """Delete all rows from database."""
-        self.cursor.execute("DELETE FROM clf_article")
-        self.cursor.execute("ALTER TABLE clf_article AUTO_INCREMENT = 0")
+        self.cursor.execute("DELETE FROM %s", (self.table_name))
+        self.cursor.execute("ALTER TABLE %s AUTO_INCREMENT = 0",
+                            (self.table_name))
         self.conn.commit()
         print("Successfully Deleted")
 
